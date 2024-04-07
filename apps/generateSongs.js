ebb18decc6146092e08c9c70bce2a277d3a2d128
ZeroCal Su <3146312184@qq.com>
2024-04-07 14:51:00 +0800

    优化细节

--- apps/generateSongs.js
+++ apps/generateSongs.js
@@ -251,55 +251,52 @@ async function setupTimeout(e) {
 }
 
 async function sendFile(e, filePath) {
+    const send_type = await Config.getConfig().send_type;
 
-    const send_type = await Config.getConfig().send_type
+    logger.info(filePath);
 
-    logger.info(filePath)
-
-    // 获取两首歌的文件名
     const songNames = Object.keys(filePath);
 
-    // 遍历
-    songNames.forEach(async (songName) => {
+    for (const songName of songNames) {
+        await e.reply(`正在发送第${songNames.indexOf(songName) + 1}首歌曲，歌曲名称：《${songName}》`);
 
-        await e.reply(`正在发送第${songNames.indexOf(songName) + 1}首歌曲，歌曲名称：《${songName}》`)
+        const checkFileExists = (path) => {
+            if (!fs.existsSync(path)) {
+                e.reply('文件不存在，请检查配置项是否开启保存对应的功能', true);
+                return false;
+            }
+            return true;
+        };
 
-        switch (send_type) {
-            case 'record':
-                // 发送语音
-                if (!fs.existsSync(filePath[songName].mp3Path)) {
-                    e.reply('文件不存在，请检查配置项是否开启保存音频功能', true);
-                    return true;
-                }
-                await e.reply(segment.record(filePath[songName].mp3Path));
-                break;
-            case 'video':
-                // 发送视频
-                if (!fs.existsSync(filePath[songName].mp4Path)) {
-                    e.reply('文件不存在，请检查配置项是否开启保存视频功能', true);
-                    return true;
-                }
-                await e.reply(segment.video(filePath[songName].mp4Path));
-                break;
-            case 'file':
-                // 发送文件
-                if (!fs.existsSync(filePath[songName].mp3Path)) {
-                    e.reply('文件不存在，请检查配置项是否开启保存音频功能', true);
-                    return true;
-                }
-                try {
-                    const upload = e.isGroup ? e.group.sendFile ? await e.group.sendFile(filePath[songName].mp3Path) : await e.group.fs.upload(filePath[songName].mp3Path) : await e.friend.sendFile(filePath[songName].mp3Path)
-                    let fileUrl = await e.group?.getFileUrl(upload.fid) || await e.friend.getFileUrl(upload)
-                    if (fileUrl) await e.reply(fileUrl)
-                    else await e.reply(JSON.stringify(upload))
-                } catch (err) {
-                    logger.error(err)
-                    e.reply('发送文件失败，请检查控制台输出再试')
-                }
-                break;
-            default:
-                break;
+        try {
+            switch (send_type) {
+                case 'record':
+                    if (checkFileExists(filePath[songName].mp3Path)) {
+                        await e.reply(segment.record(filePath[songName].mp3Path));
+                    }
+                    break;
+                case 'video':
+                    if (checkFileExists(filePath[songName].mp4Path)) {
+                        await e.reply(segment.video(filePath[songName].mp4Path));
+                    }
+                    break;
+                case 'file':
+                    if (checkFileExists(filePath[songName].mp3Path)) {
+                        const uploadMethod = e.isGroup ? e.group.sendFile || e.group.fs.upload : e.friend.sendFile;
+                        const upload = await uploadMethod(filePath[songName].mp3Path);
+                        const fileUrlMethod = e.isGroup ? e.group.getFileUrl : e.friend.getFileUrl;
+                        const fileUrl = await fileUrlMethod(upload);
+                        await e.reply(fileUrl || JSON.stringify(upload));
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } catch (err) {
+            logger.error(err);
+            await e.reply('发送文件失败，请检查控制台输出再试');
         }
-    })
-    return true
+    }
+
+    return true;
 }
